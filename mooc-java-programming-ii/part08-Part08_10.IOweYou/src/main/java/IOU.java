47589cda0abba89d8da84273a4add47c15a6fc94
beatrizmcbarbosa <92396159+beatrizmcbarbosa@users.noreply.github.com>
2022-07-08 19:58:02 +0100

    Add files via upload

---
+++ mooc-java-programming-ii/part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+import javax.naming.ldap.HasControls;
+
+public class IOU {
+
+    private HashMap<String, Double> debts;
+
+    public IOU() {
+        this.debts = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        this.debts.put(toWhom, amount);
+
+    }
+
+    public double howMuchDoIOweTo(String toWhom) {
+        return this.debts.getOrDefault(toWhom, 0.0);
+    }
+}
